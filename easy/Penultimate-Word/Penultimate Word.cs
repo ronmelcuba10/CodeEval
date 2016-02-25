818a168e126708bb68ca715b8f77a28a6745a56a
Ronny <roncareer76@yahoo.com>
2016-02-25 07:31:00 -0500

    Create Penultimate Word.cs

---
+++ easy/Penultimate-Word/Penultimate Word.cs
@@ -0,0 +1,23 @@
+using System.IO;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        using (StreamReader reader = File.OpenText(args[0]))
+        while (!reader.EndOfStream)
+        {
+            string line = reader.ReadLine();
+            if (null == line)
+                continue;
+            ShowWord(line);
+            // do something with line
+        }
+    }
+    
+     public static void ShowWord(string line){
+            string[] result = line.Split(' ');
+            System.Console.WriteLine(result[result.Length-2].Trim());
+    }
+}
