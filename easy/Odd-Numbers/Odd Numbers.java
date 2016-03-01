6f73a0f03dd37e37d682d76a5cac43469c5e44a5
Ronny <roncareer76@yahoo.com>
2016-03-01 07:27:58 -0500

    Create Odd Numbers.java

---
+++ easy/Odd-Numbers/Odd Numbers.java
@@ -0,0 +1,8 @@
+import java.io.*;
+public class Main {
+    public static void main (String[] args) throws IOException {
+        for(int i=0;i<100;i++){ 
+            if(i%2!=0) System.out.println(i);
+        }
+    }
+}
