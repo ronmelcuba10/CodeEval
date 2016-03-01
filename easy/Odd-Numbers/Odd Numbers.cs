9471c403bfd37e49e242f9d4e2508eb1288dac9d
Ronny <roncareer76@yahoo.com>
2016-03-01 07:29:06 -0500

    Create Odd Numbers.cs

---
+++ easy/Odd-Numbers/Odd Numbers.cs
@@ -0,0 +1,12 @@
+using System.IO;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        for(int i=1;i<100;i++){
+            if (i%2==1) System.Console.WriteLine(i);
+        }
+    }
+}
