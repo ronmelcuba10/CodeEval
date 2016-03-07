59361e4ec7c71e60161e77c25f595bdfb71e9ef5
Ronny <roncareer76@yahoo.com>
2016-03-07 08:35:28 -0500

    Create File-Size.java

---
+++ easy/File-Size/File-Size.java
@@ -0,0 +1,7 @@
+import java.io.*;
+public class Main {
+    public static void main (String[] args) throws IOException {
+        File file = new File(args[0]);
+        System.out.println(file.length());
+    }
+}
